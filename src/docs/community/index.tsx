4622187df2bb38a690bbbc64a6d9b904f1e31a79
Peerchemist <dx.1178@gmail.com>
2022-01-27 11:26:03 +0100

    futher cleanup

--- src/docs/community/index.tsx
+++ src/docs/community/index.tsx
@@ -1,8 +1,7 @@
 import Bugs from './bugs.mdx';
 import FAQ from './faq.mdx';
-import Features from './features.mdx';
 import License from './license.mdx';
 import Home from './home.mdx';
 import Attributions from './attributions.mdx';
 
-export { Bugs, FAQ, Features, License, Home, Attributions };
+export { Bugs, FAQ, License, Home, Attributions };
