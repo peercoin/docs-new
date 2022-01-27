5898f53ca96dd008118702dddd83bdb2e2ef20a4
Peerchemist <dx.1178@gmail.com>
2022-01-27 12:42:40 +0100

    more cleanup

--- src/docs/community/index.tsx
+++ src/docs/community/index.tsx
@@ -1,7 +1,5 @@
 import Bugs from './bugs.mdx';
 import FAQ from './faq.mdx';
-import License from './license.mdx';
 import Home from './home.mdx';
-import Attributions from './attributions.mdx';
 
-export { Bugs, FAQ, License, Home, Attributions };
+export { Bugs, FAQ, Home };
