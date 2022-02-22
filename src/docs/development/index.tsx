9c974eb2b131f4e871f80422f29d91f0e1ce224a
Muser Yeung <muser-yeung@v.systems>
2022-02-22 13:57:55 +0800

    remove missing files from build

--- src/docs/development/index.tsx
+++ src/docs/development/index.tsx
@@ -1,7 +1,5 @@
-import * as MobileFrameworks from './frameworks-mobile';
-import * as WebFrameworks from './frameworks-web';
 import Environment from './environment.mdx';
 import Testing from './testing.mdx';
 import CLI from './cli.mdx';
 
-export { MobileFrameworks, WebFrameworks, Environment, Testing, CLI };
+export { Environment, Testing, CLI };
