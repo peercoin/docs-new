c4f0b0cebee1d46b62fd135a759d8b4986efe3d2
Willy <willy@coinerella.com>
2022-05-22 22:34:44 +0200

    foundation / university img update

--- src/__configuration__/landingPage/cardData.ts
+++ src/__configuration__/landingPage/cardData.ts
@@ -1,10 +1,17 @@
-import components from '../../../src/app/assets/card-images/components.png';
+import foundation from '../../../src/app/assets/card-images/foundation.png';
 import github from '../../../src/app/assets/card-images/github.png';
-import resources from '../../../src/app/assets/card-images/resources.png';
+import university from '../../../src/app/assets/card-images/university.png';
 import community from '../../../src/app/assets/card-images/community.png';
 import roadmap from '../../../src/app/assets/card-images/roadmap.png';
+import components from '../../../src/app/assets/card-images/components.png';
 
 export const cardData = [
+    {
+        title: 'Open Documentation',
+        description: `Takes you right to the Docs.`,
+        image: components,
+        path: '/overview',
+    },
     {
         title: 'Peercoin Website',
         description: `Check out the main Peercoin website and find all kinds of additional information.`,
@@ -14,13 +21,13 @@ export const cardData = [
     {
         title: 'Peercoin Foundation',
         description: `The Peercoin Foundation has been established with the simple mission of promoting and supporting the continued education, development, and overall progression of the Peercoin project.`,
-        image: components,
+        image: foundation,
         path: 'https://www.peercoin.net/foundation',
     },
     {
         title: 'Peercoin University',
         description: `Peercoin university is a community project aimed at less technical members of the community to grasp and understand the complex topic of public blockchain.`,
-        image: resources,
+        image: university,
         path: 'https://www.peercoin.net/university',
     },
     {
